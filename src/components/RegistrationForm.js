b8109bbe5bb9cb0a74f9e036facac279207ce305
vivedha-2306 <vivedha.a.2023.cse@ritchennai.edu.in>
2026-02-16 14:53:20 +0530

    Update RegistrationForm.js

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-const API_BASE = 'http://127.0.0.1:5000';
+const API_BASE = 'https://backend-ov13.onrender.com';
 
 export default function RegistrationForm({ eventId }) {
   const [formData, setFormData] = useState({ name: '', email: '', phone: '' });
